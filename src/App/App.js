29ae03571536257c14285cccbf5ce9a078625d64
sanjay rajpoot <iamsanjayrajpoot@gmail.com>
2020-12-21 17:35:49 +0530

    Added products to site

--- src/App/App.js
+++ src/App/App.js
@@ -36,9 +36,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <div className="App-main" >
-              <Product /> 
-          </div>
+          <div className="container App-main" >
+              <div className="row">
+              <Product price="5.12" title="Gun " imgUrl="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.indiamart.com%2Fproddetail%2Fsoft-bullet-toy-gun-17091725833.html&psig=AOvVaw2w8XOf--94xoXsIFXWHSxF&ust=1608637873407000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNi395mB3-0CFQAAAAAdAAAAABAD"/> 
+              </div>              
+          </div>   
         </header>
       </div>
     );
--- src/product/product.js
+++ src/product/product.js
@@ -4,12 +4,12 @@ import './product.css';
 class Product extends Component(){
     render(){   // render= show on screen
         return(
-            <div className="card">
-                <img className="card-img-top" alt="Product">  </img>
+            <div className="card Product">
+                <img className="card-img-top" src={this.props.imgUrl} alt="Product">  </img>
                 <div className="card-block">
-                    <h4 className="card-title">  </h4>
-                    <p className="card-text">Price: $</p>
-                    <a href="#" className="btn btn-primary">Add to WishList</a>
+                    <h4 className="card-title" src={this.props.title}>  </h4>
+                    <p className="card-text">Price: ${this.props.price}</p>
+                    <a href="#" className="btn btn-primary">Add to WishList. </a>
 
                 </div>
             </div>
